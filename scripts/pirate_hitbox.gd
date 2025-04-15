e00e5f87c13667dc62d0c62b06c72afd7fc020ca
Amodh Dhakal <adhakal2@ncsu.edu>
2025-04-14 23:57:56 -0400

    Handle damage for pirates

--- scripts/pirate_hitbox.gd
+++ scripts/pirate_hitbox.gd
@@ -1,17 +1,35 @@
 extends Area2D
 
 signal PirateClicked
+signal DamageTaken
 
 var bloodTexture = preload("res://images/bloodpirate.png")
+var isClicked = false
+
 @onready var pirate = $Darkpiratesword
 @onready var collision = $CollisionPolygon2D
+@onready var timer = $Timer
 
-func _ready():
+@export var texture: Texture2D
+@export var heartsDamaged: int
+@export var timeForDamage: float
+
+func _ready() -> void:
+	pirate.texture = texture
 	input_pickable = true
+
+	timer.wait_time = timeForDamage
+	timer.start()
+	
+func _on_timer_timeout() -> void:
+	if not isClicked:
+		emit_signal("DamageTaken", heartsDamaged)
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			pirate.texture = bloodTexture
 			collision.disabled = true
+			isClicked = true
+			timer.stop()
 			emit_signal("PirateClicked")
--- scripts/second_game.gd
+++ scripts/second_game.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 @onready var HealthLabel = $HealthLabel
 @onready var Pirate = preload("res://components/Pirate.tscn")
-
-var crosshair_texture = preload("res://images/crosshair.png")
+@onready var crosshair_texture = preload("res://images/crosshair.png")
 var crosshair_sprite: Sprite2D
 
 const VIEWPORT_WIDTH = 1152
@@ -12,9 +11,12 @@ const X_BOUNDARY = 150
 const Y_BOUNDARY = 150
 
 var piratesRemaining = 38
-var playerHealth = 10
+var playerHealth = 5
 var isPirateVisible = false
 var currentPirate: Area2D
+
+const swordPaths = ["res://images/bandanapiratesword.png", "res://images/darkpiratesword.png", "res://images/lightpiratesword.png"]
+const gunPaths = ["res://images/darkpirategun.png", "res://images/lightpirategun.png", "res://images/lightpirateguntwo.png"]
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -31,13 +33,36 @@ func _process(delta):
 		var chosenWidth = randi() % (VIEWPORT_WIDTH - X_BOUNDARY) + X_BOUNDARY
 		var chosenHeight = randi() % (VIEWPORT_HEIGHT - Y_BOUNDARY) + Y_BOUNDARY
 		
+		var swordChosen = [true, false].pick_random()
+		var chosenTexture = load((swordPaths if swordChosen else gunPaths).pick_random())
+		var chosenHeartsDamaged = 1 if swordChosen else 2
+		var timeForDamage = 1 if swordChosen else 2
+		
 		currentPirate = Pirate.instantiate()
+		currentPirate.texture = chosenTexture
+		currentPirate.heartsDamaged = chosenHeartsDamaged
+		currentPirate.timeForDamage = timeForDamage
+		
 		currentPirate.position = Vector2(chosenWidth, chosenHeight)
 		currentPirate.PirateClicked.connect(onPirateClicked)
+		currentPirate.DamageTaken.connect(onPirateDamageTaken)
 		add_child(currentPirate)
 		
 		piratesRemaining -= 1
 		isPirateVisible = true
+
+func onPirateDamageTaken(heartsDamaged):
+	playerHealth -= heartsDamaged
+	currentPirate.queue_free()
+	isPirateVisible = false
+	
+	print("Damage taken")
+	
+	if (playerHealth < 0):
+		print("Game lost")
+		# TODO: Handle player loss
+	
+	displayPlayerHealth()
 		
 func onPirateClicked():
 	await get_tree().create_timer(0.75).timeout 
@@ -45,9 +70,11 @@ func onPirateClicked():
 	isPirateVisible = false
 	piratesRemaining -= 1
 	
+	print("Clicked on time")
+	
 	if ( piratesRemaining < 0):
-		print("Game over")
-		# TODO: Handle game over
+		print("Game win")
+		# TODO: Handle game win
 
 
 func displayPlayerHealth():
