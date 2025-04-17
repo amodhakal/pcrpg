f90e172ec749ec29af98e05b3e30cbcb286f0561
gdlamb <gdlamb@ncsu.edu>
2025-04-17 19:34:26 -0400

    Fixed music and sound effects in first game

--- scripts/ToFirst.gd
+++ scripts/ToFirst.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var FIRST_GAME = "res://scenes/FirstGame.tscn"
 
-func _on_button_pressed() -> void:
+func _ready():
 	AudioManager.playMusic("first")
+
+func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file(FIRST_GAME)
