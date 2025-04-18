3fcfa05148c10519e64cc55fdf818caf0ea30183
Amodh Dhakal <adhakal2@ncsu.edu>
2025-04-17 20:03:53 -0400

    Added opening music

--- scenes/opening_scene.gd
+++ scenes/opening_scene.gd
@@ -6,9 +6,10 @@ var ToFirstPath = "res://scenes/ToFirst.tscn"
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	AudioManager.playMusic("tension")
 
 func _process(delta: float) -> void:
-	Scrolled.position.y -= delta * 20
+	Scrolled.position.y -= delta * 30
 	
-	if ( Scrolled.position.y < -900):
+	if ( Scrolled.position.y < -1100):
 		get_tree().change_scene_to_file(ToFirstPath)
